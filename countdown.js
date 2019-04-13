8019f18745df95fdfee5bcb15d58482926b8191f
Luke Rollins <lukerllns@gmail.com>
2019-04-13 01:30:18 -0400

    Wanna be safe and sane so I'm committing like I should've done last night

---
+++ countdown.js
@@ -0,0 +1,56 @@
+let countdown;
+const timerDisplay = document.querySelector('.display__time-left');
+const alarm = new Audio("Klaxon_Alarm_Sound_Effect.mp3")
+
+
+function timer(seconds) {
+
+    clearInterval(countdown);
+
+
+    const now = Date.now();
+    const then = now + seconds * 1000;
+    displayTimeLeft(seconds);
+    
+    countdown = setInterval(() => {
+       const secondsLeft = Math.round((then - Date.now()) / 1000);
+       if(secondsLeft < 0) {
+           clearInterval(countdown);
+           document.title = "Your alarm is going off!!!"
+           timerDisplay.textContent = "Ding! Fries are done!!!";
+           alarm.play();
+           return;
+       }
+       displayTimeLeft(secondsLeft);
+    }, 1000);
+}
+
+function displayTimeLeft(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainderSeconds = seconds % 60;
+    const display = `${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`;
+    timerDisplay.textContent = display;
+    console.log(display);
+}
+
+function startTimer() {
+    const seconds = parseInt(this.dataset.time);
+    timer(seconds);
+}
+
+function clearing() {
+    timerDisplay.textContent = " ";
+    //endTime.textContent = " ";
+    document.title = "Stop! Reset!";
+    alarm.pause();
+    clearInterval(countdown);
+}
+
+document.getElementById('reset_button').addEventListener('click', clearing);
+document.getElementById('start_button').addEventListener('click', startTimer);
+document.customForm.addEventListener('submit', function(e) {
+    e.preventDefault();
+    const mins = this.minutes.value;
+    timer(mins * 60);
+    this.reset();
+});
